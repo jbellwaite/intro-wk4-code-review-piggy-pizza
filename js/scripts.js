a758ea1ea532feb3b7a7ffdef98c80d62454f4a6
Jessie <jess.bell@me.com>
2017-09-29 15:54:17 -0700

    Add functionality to add additional form and price for delivery option, but for time considerations, will be commented out

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,28 @@ function PizzaOrder(name, meat, cheese, veggies, sauce, size) {
   this.userSize = size;
 }
 
+// function AddDelivery(delivery, address) {
+//   this.userDelivery = delivery;
+//   this.userAddress = address;
+// }
+
 PizzaOrder.prototype.pizzaPrice = function () {
-  if (this.userSize === "Small") {
+  if (this.userSize === "small") {
     return "10";
-  }else if (this.userSize === "Medium") {
+  }else if (this.userSize === "medium") {
     return "15";
   }else {
     return "20";
   }
 }
 
+PizzaOrder.prototype.lowerCase = function () {
+  this.userMeat = this.userMeat.toLowerCase();
+  this.userCheese = this.userCheese.toLowerCase();
+  this.userSize = this.userSize.toLowerCase();
+  this.userVeggies = this.userVeggies.toLowerCase();
+  this.userSauce = this.userSauce.toLowerCase();
+}
 
 
 $(document).ready(function() {
@@ -30,7 +42,7 @@ $(document).ready(function() {
     var size = $("#size").val();
 
     var pizzaStyle = new PizzaOrder(name, meat, cheese, veggies, sauce, size);
-
+    pizzaStyle.lowerCase();
     $("#output").show();
     $("#order-recap").text(pizzaStyle.userName +
       ", your order for a " +
@@ -45,6 +57,14 @@ $(document).ready(function() {
       pizzaStyle.userSauce
     );
     $("#cost").text(pizzaStyle.pizzaPrice());
-
   });
 });
+
+    // var delivery = $("input:radio[name=delivery]:checked").val();
+    //   if (delivery === "delivery") {
+    //     $("#address").show();
+    //   }
+    // });
+    // $
+    //   var address = $("#address").val();
+    // var pizzaDelivery = new AddDelivery(delivery, address);
